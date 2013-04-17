719656c0cf8242e5b4125c918d9387619506fcd2
BorisKozo <majorzbzzn@gmail.com>
2013-04-17 16:57:01 +0300

    Added app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+﻿var express = require('express');
+var app = express();
+
+app.get("/hello_world", function (req, res) {
+    res.send("<h1>Hello World</h1>");
+});
+
+app.listen(3000);
+console.log('Listening on port 3000');
